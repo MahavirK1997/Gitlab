f2f825c6d19bbe996d6e3475ff1f23fe1fc9ccec
MahavirK1997 <kathed.m@northeastern.edu>
2023-10-09 15:00:53 -0400

    add the print

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 print("hello everyone")
-<<<<<<< HEAD
 print("hello world")
 
+print("i am okay"
+      )
 
 print("how are you")
-=======
 print("hello")
->>>>>>> 92afac337c91b4b9e46dce488d3fad35b21e13ae
